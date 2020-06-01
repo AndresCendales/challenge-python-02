2cbc631678c4a6974382a9e58e0eda7177dfa202
Andres Cendales <andres01660@gmail.com>
2020-05-31 21:49:40 -0500

    Challenge completado 100%

--- src/main.py
+++ src/main.py
@@ -1,15 +1,52 @@
 # Resolve the problem!!
 import string
+import random
 
 SYMBOLS = list('!"#$%&\'()*+,-./:;?@[]^_`{|}~')
 
 
 def generate_password():
-    # Start coding here
+    # Define el largo de la clave
+    lenght = random.randint(8,16)
+    
+    #inicializamos la clave y variables para pre-validacion
+    password = []
+    has_low = False
+    has_upper = False
+    has_digit = False
+    has_symbol = False
+    
+    #Loop que se repite a menos que la contrasena sea segura
+    while True:
+        for idx in range(lenght):
+            type_char = random.randint(0,3)
 
+            if type_char == 0:
+                password.append(chr(random.randint(97,122)))
+                has_low = True
+            elif type_char ==  1:
+                password.append(chr(random.randint(65,90)))
+                has_upper = True
+            elif type_char == 2:
+                password.append(chr(random.randint(48,57)))
+                has_digit = True
+            elif type_char == 3:
+                password.append(SYMBOLS[random.randint(0,len(SYMBOLS)-1)])
+                has_symbol = True
+        
+        #Pre-validacion para asegurarse que contiene al menos un caracter de cada tipo (minuscula, mayuscula, digito y simbolo)
+        if has_low == False or has_upper == False or has_digit == False or has_symbol == False:
+            has_low = False
+            has_upper = False
+            has_digit = False
+            has_symbol = False
+            del password[:]
+        else: 
+            break  
+        
+    return ''.join(password)
 
 def validate(password):
-
     if len(password) >= 8 and len(password) <= 16:
         has_lowercase_letters = False
         has_numbers = False
@@ -43,6 +80,7 @@ def validate(password):
 
 def run():
     password = generate_password()
+
     if validate(password):
         print('Secure Password')
     else:
